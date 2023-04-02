199291f5155ddc4762a858bf5329bc5d412abe1b
unknown <ytbcata@gmail.com>
2023-04-02 14:54:16 +0300

    Final Version webiste 1.0

--- src/App.js
+++ src/App.js
@@ -9,16 +9,12 @@ import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-    
-    <Routes>
-
-      <Route path="/" element={<Home/>} /> 
-      <Route path="/project" element={<Project/>} /> 
-      <Route path="/about" element={<About/>} /> 
-      <Route path="/contact" element={<Contact/>} /> 
-
-    </Routes>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
