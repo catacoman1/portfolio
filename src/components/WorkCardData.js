7e3cdb42f71913c1d785d312a05f057448e15271
unknown <ytbcata@gmail.com>
2023-04-11 01:36:08 +0300

    added one project

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,14 +1,15 @@
 import { getByTitle } from "@testing-library/react"
-import pro1 from "../assets/coming_soon.png"
+
 import pro2 from "../assets/coming_soon.png"
 import pro3 from "../assets/coming_soon.png"
+import proiect1 from "../assets/proiect1.png"
 
 const ProjectCardData = [
     {
-        imgsrc: pro1,
-        title: "Coming Soon",
-        text: "Working on more projects. I will add them to this Portfolio once i think they are good enough.",
-        view: "https://github.com/catacoman1"
+        imgsrc: proiect1,
+        title: "ToDo List",
+        text: "Simple ToDo List done using ReactJs and Bootstrap. This simple application is helping me to keep tracking of activities during the day.",
+        view: "https://github.com/catacoman1/todolist"
     },
     {
         imgsrc: pro2,
